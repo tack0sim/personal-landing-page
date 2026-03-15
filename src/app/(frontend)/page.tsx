6f76007b45738e50d8774162cbd523be8501a7e9
tack0sim <talhamjd@gmail.com>
2026-03-15 23:29:28 +0100

    feat: seo image

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,4 +1,5 @@
 import { PageBuilder } from '@/components/pagebuilder';
+import { resolveImageDimensions, urlFor } from '@/sanity/lib/image';
 import { sanityFetch } from '@/sanity/lib/live';
 import { HOMEPAGE_QUERY, SETTINGS_QUERY } from '@/sanity/queries';
 import type { Metadata } from 'next';
@@ -21,16 +22,31 @@ export async function generateMetadata(): Promise<Metadata> {
     ? `${metadata.title} | ${settings.siteTitle}`
     : settings?.siteTitle;
 
+  const imageUrl = metadata?.og?.image && urlFor(metadata.og.image).url();
+  const dimensions =
+    metadata?.og?.image?.image && resolveImageDimensions(metadata?.og?.image);
+
   return {
     title,
     description: metadata?.seo?.description ?? settings?.seo?.description,
     openGraph: {
       title: metadata?.og?.title ?? settings?.og?.title,
       description: metadata?.og?.description ?? settings?.og?.description,
-      images: [],
+      images: [
+        {
+          url: imageUrl as string,
+          alt: metadata?.og?.image?.alt as string,
+          width: dimensions?.width,
+          height: dimensions?.height,
+        },
+      ],
     },
     alternates: {
       canonical: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/`,
+    },
+    robots: {
+      index: metadata?.seo?.noIndex ? false : true,
+      follow: metadata?.seo?.noIndex ? false : true,
     },
   };
 }
