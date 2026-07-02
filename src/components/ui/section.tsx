e82ad79b19fa6c99f0eaa47b4092cf2854f49a1e
tack0sim <talhamjd@gmail.com>
2026-07-02 15:55:38 +0200

    style: mobile section padding

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -9,7 +9,7 @@ export function Section({
 }) {
   return (
     <section
-      className={cn('w-full mx-auto py-20', className)}
+      className={cn('w-full mx-auto py-12 lg:py-20', className)}
     >
       {children}
     </section>
